d9b9c25bf0afccac337ee1cae1cd0efc1f7a6c91
lucasgianine <lucas.esilva@sptech.school>
2023-11-23 12:28:40 -0300

    feat: Iniciando framework

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import SearchProfile from './view/components/SearchProfile'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <SearchProfile />
+    </>
+  )
+}
+
+export default App
